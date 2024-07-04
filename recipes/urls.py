48ecf2965090f1a21e93efb13b6eacdec4fa978b
Jake Cunningham <jakecunningham1910@gmail.com>
2024-07-04 14:57:14 +0100

    Delete recipe functionality

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddRecipe, Recipes, RecipesDetail
+from .views import AddRecipe, Recipes, RecipesDetail, DeleteRecipe
 
 urlpatterns = [
     path("", AddRecipe.as_view(), name="add_recipe"),
     path("recipes/", Recipes.as_view(), name="recipes"),
     path("<int:pk>/", RecipesDetail.as_view(), name="recipe_detail"),
+    path('delete/<int:pk>/', DeleteRecipe.as_view(), name='delete_recipe'),
 ]
