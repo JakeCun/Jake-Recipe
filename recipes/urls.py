310e9826e38494f4ded70a17245a4976b00c1270
Jake Cunningham <jakecunningham1910@gmail.com>
2024-07-04 15:15:32 +0100

    Add edit recipe functionality

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddRecipe, Recipes, RecipesDetail, DeleteRecipe
+from .views import AddRecipe, Recipes, RecipesDetail, DeleteRecipe, EditRecipe
 
 urlpatterns = [
-    path("", AddRecipe.as_view(), name="add_recipe"),
-    path("recipes/", Recipes.as_view(), name="recipes"),
+    path("add/", AddRecipe.as_view(), name="add_recipe"),
+    path("", Recipes.as_view(), name="recipes"),
     path("<int:pk>/", RecipesDetail.as_view(), name="recipe_detail"),
     path('delete/<int:pk>/', DeleteRecipe.as_view(), name='delete_recipe'),
+    path('edit/<int:pk>/', EditRecipe.as_view(), name='edit_recipe'),
 ]
