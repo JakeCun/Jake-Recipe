9049b1d271f756c22b6da884e82a377fc23e2807
Jake Cunningham <jakecunningham1910@gmail.com>
2024-07-05 02:14:36 +0100

    Add profile information page

--- profiles/models.py
+++ profiles/models.py
@@ -7,20 +7,26 @@ from django.dispatch import receiver
 
 from django_resized import ResizedImageField
 
+
 class Profile(models.Model):
-    """ Profile model """
-    user= models.ForeignKey(User, related_name="profiles", on_delete=models.CASCADE)
-    image = ResizedImageField(size = [300,300], quality=75, upload_to="profiles/", force_format="WEBP", blank=False)
+    """Profile model"""
+
+    user = models.ForeignKey(User, related_name="profiles", on_delete=models.CASCADE)
+    image = ResizedImageField(
+        size=[300, 300],
+        quality=75,
+        upload_to="profiles/",
+        force_format="WEBP",
+        blank=False,
+    )
     bio = RichTextField(max_length=2500, null=True, blank=True)
 
     def __str__(self):
         return str(self.user.username)
-    
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(instance, created, **kwargs):
-    """ Create or update a profile """
+    """Create or update a profile"""
     if created:
         Profile.objects.create(user=instance)
-    
