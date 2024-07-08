8c0abd8b0c6364050de00b966a1d9753698637f5
Jake Cunningham <jakecunningham1910@gmail.com>
2024-07-08 13:14:15 +0100

    Updated env.py with new configurations

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["SECRET_KEY"] = 'jvkfldjio*(^Y*IGYIfidt34'
+os.environ["DEVELOPMENT"] = "True"
+os.environ["CLOUDINARY_URL"] = 'cloudinary://752899158566725:OA0kFtcwBM7eoOJK_t3zaUBHIis@dks4smpdt'
+os.environ['DATABASE_URL'] = 'postgres://uat3lvtdsc7tdv:pf93360aeab9707d00f992e20a44978fcc2cbf0b5cc64013ab55e331c273d4af2@c7u1tn6bvvsodf.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d2un0jqe9n2dqf'
